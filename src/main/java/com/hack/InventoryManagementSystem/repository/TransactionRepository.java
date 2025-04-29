8ab7a6d3ac0a9b09cfdc029db3b040ecedd6a628
Camilo Carvajal <cacarvajalh@correo.udistrital.edu.co>
2025-04-28 21:14:49 -0500

    Ajuste de filtro

--- src/main/java/com/hack/InventoryManagementSystem/repository/TransactionRepository.java
+++ src/main/java/com/hack/InventoryManagementSystem/repository/TransactionRepository.java
@@ -19,10 +19,11 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
     //we are searching these field; Transaction's description, note, status, Product's name, sku
     @Query("SELECT t FROM Transaction t " +
             "LEFT JOIN t.product p " +
-            "WHERE (:searchText IS NULL OR " +
+            "WHERE (:searchText = '' OR :searchText IS NULL OR " +
             "LOWER(t.description) LIKE LOWER(CONCAT('%', :searchText, '%')) OR " +
             "LOWER(t.status) LIKE LOWER(CONCAT('%', :searchText, '%')) OR " +
             "LOWER(p.name) LIKE LOWER(CONCAT('%', :searchText, '%')) OR " +
             "LOWER(p.sku) LIKE LOWER(CONCAT('%', :searchText, '%')))")
     Page<Transaction> searchTransactions(@Param("searchText") String searchText, Pageable pageable);
+
 }
