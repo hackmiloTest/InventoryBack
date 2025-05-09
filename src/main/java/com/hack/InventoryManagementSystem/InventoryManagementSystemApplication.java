a7ef5e99dad3f3461ae2e6c9b6c0dd26082af951
Camilo Carvajal <cacarvajalh@correo.udistrital.edu.co>
2025-05-08 19:42:01 -0500

    Ajuste de horario de ventas y transacciones

--- src/main/java/com/hack/InventoryManagementSystem/InventoryManagementSystemApplication.java
+++ src/main/java/com/hack/InventoryManagementSystem/InventoryManagementSystemApplication.java
@@ -1,10 +1,18 @@
 package com.hack.InventoryManagementSystem;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class InventoryManagementSystemApplication {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Bogota"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryManagementSystemApplication.class, args);
