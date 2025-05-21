3da5c7c767332d69c9ec19c4a74b20826e8260f2
Camilo Carvajal <cacarvajalh@correo.udistrital.edu.co>
2025-05-21 10:24:57 -0500

    ajuste transactions 3

--- src/main/java/com/hack/InventoryManagementSystem/controller/TransactionController.java
+++ src/main/java/com/hack/InventoryManagementSystem/controller/TransactionController.java
@@ -35,7 +35,7 @@ public class TransactionController {
     public ResponseEntity<Response> getAllTransactions(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "1000") int size,
-            @RequestParam(required = false) String searchText
+            @RequestParam(required = false, defaultValue = "") String searchText
     ) {
         return ResponseEntity.ok(transactionService.getAllTransactions(page, size, searchText));
     }
