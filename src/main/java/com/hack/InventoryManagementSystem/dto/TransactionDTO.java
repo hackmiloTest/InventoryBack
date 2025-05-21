418588a3750cc7ccd8a942b54c04ea72c0550fb6
Camilo Carvajal <cacarvajalh@correo.udistrital.edu.co>
2025-05-21 18:23:27 -0500

    ajuste transactions 4

--- src/main/java/com/hack/InventoryManagementSystem/dto/TransactionDTO.java
+++ src/main/java/com/hack/InventoryManagementSystem/dto/TransactionDTO.java
@@ -1,47 +1,45 @@
-package com.hack.InventoryManagementSystem.dto;
+    package com.hack.InventoryManagementSystem.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.hack.InventoryManagementSystem.enums.TransactionType;
-import com.hack.InventoryManagementSystem.enums.TransactionStatus;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+    import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+    import com.fasterxml.jackson.annotation.JsonInclude;
+    import com.hack.InventoryManagementSystem.enums.TransactionType;
+    import com.hack.InventoryManagementSystem.enums.TransactionStatus;
+    import jakarta.persistence.*;
+    import lombok.AllArgsConstructor;
+    import lombok.Data;
+    import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
+    import java.math.BigDecimal;
+    import java.time.LocalDateTime;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class TransactionDTO {
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public class TransactionDTO {
 
-    private Long id;
+        private Long id;
 
-    private Integer totalProducts;
+        private Integer totalProducts;
 
-    private BigDecimal totalPrice;
+        private BigDecimal totalPrice;
 
-    private TransactionType transactionType;
+        private TransactionType transactionType;
 
-    private TransactionStatus status;
+        private TransactionStatus status;
 
-    private String description;
+        private String description;
 
-    private LocalDateTime updatedAt;
+        private LocalDateTime updatedAt;
 
-    private LocalDateTime createdAt;
+        private LocalDateTime createdAt;
 
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id")
-    private ProductDTO product;
+        private ProductDTO product;
 
-    private UserDTO user;
+        private UserDTO user;
 
-    private SupplierDTO supplier;
-    private Long originalSaleId;
+        private SupplierDTO supplier;
+        private Long originalSaleId;
 
-}
+    }
